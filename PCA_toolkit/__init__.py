2a0e2f20e766932fd51ff63f4830282a89f9bb7c
Ricardo Luna <ricardo.luna@conchaytoro.cl>
2023-10-16 15:53:54 -0300

    se ha generado paquete distribuible de PCA version 1

---
+++ PCA_toolkit/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 10:33:38 2023
+
+@author: rluna
+"""
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 15:34:36 2023
+
+@author: rluna
+"""
+
+from setuptools import setup
+
+setup(
+      name = 'PCA toolkit',
+      author = 'Ricardo Luna',
+      version = '1.0',
+      description = 'Contains PCA object to generate differents plots',
+      author_email = 'ricardo.luna@conchaytoro.cl',
+      packages = ['PCA_toolkit']
+      )
